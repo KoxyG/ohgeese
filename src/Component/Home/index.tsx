0c02a53027c11871b9ff9e5dcca3bdf4be8cf9ec
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2023-10-17 18:34:48 +0100

    first commit

---
+++ src/Component/Home/index.tsx
@@ -0,0 +1,17 @@
+const Home = () => {
+  return (
+    <div className="container mx-auto">
+      <div className="text-center ">
+        <h1 className="font-black text-4xl sm:text-6xl  pt-[150px]">
+          Build Something Amazing
+        </h1>
+        <p className="text-gray-700 text-[12px] sm:text-[22px] pt-[10px]">
+          Must today firm from bag. Investment try cold a when <br /> capital.
+          Everything wait person service.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;

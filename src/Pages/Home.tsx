2698574402c507dfb127c4580360d09e4ead6c90
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2023-10-18 13:38:49 +0100

    fixed column

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -70,7 +70,7 @@ const Home = () => {
           </p>
         </div>
 
-        <div className="container  text-black mx-auto px-8 grid justify-center py-[100px] gap-4 ">
+        <div className="container  text-center text-black mx-auto px-8 grid justify-center py-[100px] gap-4 ">
           
           <div className="grid text-center  pb-[60px]">
             <h3 className="font-bold text-xl sm:text-2xl ">
@@ -94,7 +94,7 @@ const Home = () => {
             </p>
           </div>
 
-          <div className="grid ">
+          <div className="grid pb-[60px] ">
             <h3 className="font-bold text-xl sm:text-2xl ">
               Move weight here just either attorney?
             </h3>
